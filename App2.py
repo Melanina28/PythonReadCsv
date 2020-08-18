c39df4d22cf3f0cd16caba4283d066e9619ac563
Mel <melanie@thincrs.com>
2020-08-18 18:13:33 -0500

    version 2

--- App2.py
+++ App2.py
@@ -4,29 +4,22 @@ import threading
 #,index_col=None, header=None
 
 def lectura():
-    i = 0
     vec = np.array([0])
+    df = p.read_csv('https://docs.google.com/spreadsheets/d/1dVSzbkNbhTfvjSnpQgszYhOvQFjYFDQJYozFNSqPGqE/gviz/tq?tqx=out:csv&sheet=prueba')
+    matriz=np.asanyarray(df)
+    print(len(matriz))
+    vec = np.insert(vec, 1, len(matriz))
+    print(vec[1])
+    numero =  input()
+    df = p.read_csv('https://docs.google.com/spreadsheets/d/1dVSzbkNbhTfvjSnpQgszYhOvQFjYFDQJYozFNSqPGqE/gviz/tq?tqx=out:csv&sheet=prueba')
+    matriz=np.asanyarray(df)
+    vec = np.insert(vec, 2, len(matriz))
     print(vec)
-    # threading.Timer(5.0, lectura).start() 
-    
-    while True:
-        if vec[0] == 0:
-            df = p.read_csv('https://docs.google.com/spreadsheets/d/1dVSzbkNbhTfvjSnpQgszYhOvQFjYFDQJYozFNSqPGqE/gviz/tq?tqx=out:csv&sheet=prueba')
-            matriz=np.asanyarray(df)
-            # vec = np.insert(vec, 0, 1)
-            vec = np.insert(vec, 0, len(matriz))
-            print("entro primera")
-            print(vec)
-            i = i + 1
-        else:
-            if vec[0] > 0:     
-                df = p.read_excel('ejemplo2.xlsx')
-                matriz=np.asanyarray(df)
-                vec = np.insert(vec, i, len(matriz))
-                print("esta en la segunda")
-                if vec[i] > vec[i-1]:
-                    print("si jalo ")
-                    print(vec)
+    if  vec[1] < vec[2]:
+        total = vec[2]-vec[1]
+        total = int(total * -1) 
+        print(total)
+        print(matriz[total:,:5])
             
 lectura()
 
